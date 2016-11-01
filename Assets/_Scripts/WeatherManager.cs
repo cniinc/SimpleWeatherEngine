83bd2d2888f0869cf3cfcbf2ab92a5c48fcb5e5e
Jason M <software@positrongames.com>
2016-11-01 15:39:44 -0700

    Celestial Bodies - must fix sun color

--- Assets/_Scripts/WeatherManager.cs
+++ Assets/_Scripts/WeatherManager.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class WeatherManager : MonoBehaviour {
+	[Tooltip ("0 is midnight, 12 is noon, 6.5 is 6:30am")] 
+	[SerializeField] private float CurrentTime;
+
+	private enum cloudiness {none, clear, mild, clouded};
+	private cloudiness m_Cloudiness;
+	private enum dayPhase {none, night, sunrise, day, sunset}
+	private dayPhase m_DayPhase;
+
+
 
 	// Use this for initialization
 	void Start () {
