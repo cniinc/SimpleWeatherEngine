2c16c45ae5a3139fe88eb8c6be81a347f5dd3fac
Jason M <software@positrongames.com>
2016-11-02 01:24:56 -0700

    added rocks and a few other changes

--- Assets/_Scripts/WeatherManager.cs
+++ Assets/_Scripts/WeatherManager.cs
@@ -2,23 +2,45 @@
 using System.Collections;
 
 public class WeatherManager : MonoBehaviour {
-	[Tooltip ("0 is midnight, 12 is noon, 6.5 is 6:30am")] 
-	[SerializeField] private float CurrentTime;
+	[Tooltip ("0 is midnight, 12 is noon, 6.5 is sunrise, or 6:30am")] 
+	[SerializeField] private float gameTimeOfDay;
+	[Tooltip("in seconds")]
+	[SerializeField] private float totalDayLength; 
+	private float sunRoundTime = 0;
+
+	[SerializeField] private GameObject CelestialBodies;
 
 	private enum cloudiness {none, clear, mild, clouded};
 	private cloudiness m_Cloudiness;
-	private enum dayPhase {none, night, sunrise, day, sunset}
+	private enum dayPhase {none, night, sunrise, day, sunset};
 	private dayPhase m_DayPhase;
-
 
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+
+
 	// Update is called once per frame
 	void Update () {
+		
+		if (gameTimeOfDay > 24f || gameTimeOfDay < 0f) {
+			print ("time out of 24h");
+			gameTimeOfDay = 0f;
+		}
+
+		gameTimeOfDay = sunRoundTime / totalDayLength * 24;
+
+//		CelestialBodies.transform.rotation = Quaternion.FromToRotation (Vector3.down, new Vector3 (0, CurrentTime / 24 * 360, 0));
+		CelestialBodies.transform.rotation = Quaternion.Euler((sunRoundTime/totalDayLength*360 * -1) - 90, 0, 0);
+
+		if (sunRoundTime >= totalDayLength || sunRoundTime < 0f)
+			sunRoundTime = 0f;
+		else sunRoundTime += (Time.deltaTime);
+
+		print (sunRoundTime/totalDayLength);
 	
 	}
 }
