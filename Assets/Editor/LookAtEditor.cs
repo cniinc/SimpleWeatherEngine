32ac1557ef11a4bd8e42bc49df9f18e5e8eac924
Jason M <software@positrongames.com>
2016-11-01 19:35:25 -0700

    Cloud Left and Right implemented. Needs an art pass

---
+++ Assets/Editor/LookAtEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+
+
+[CustomEditor(typeof(LookAtCamera))]
+public class LookAtEditor : Editor {
+	bool AutoOrientToCamera = true;
+	public Camera ViewersCamera;
+
+	// Use this for initialization
+	void Start () {
+
+		ViewersCamera = Camera.main;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector ();
+
+		AutoOrientToCamera = EditorGUILayout.Toggle ("Auto Orient to Camera", AutoOrientToCamera);
+
+		if (AutoOrientToCamera)
+			LookAtCamera ();
+
+		LookAtCamera LookAtScript = (LookAtCamera)target;
+		if(GUILayout.Button("Orient To Camera"))
+		{
+			LookAtCamera ();
+		}
+	}
+
+	private void LookAtCamera()
+	{
+		LookAtCamera LookAtScript = (LookAtCamera)target;
+		if (!ViewersCamera)
+			ViewersCamera = Camera.main;
+
+		LookAtScript.LookAt(ViewersCamera.gameObject);
+	}
+}
+#endif
